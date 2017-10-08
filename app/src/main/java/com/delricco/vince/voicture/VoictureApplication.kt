713c43f3e7a37fdfd5d08e248a362d47d84b986b
vdelricco <vd12scbc@gmail.com>
2017-10-08 17:46:26 -0600

    Add leakcanary

--- app/src/main/java/com/delricco/vince/voicture/VoictureApplication.kt
+++ app/src/main/java/com/delricco/vince/voicture/VoictureApplication.kt
@@ -2,11 +2,18 @@ package com.delricco.vince.voicture
 
 import android.app.Application
 import com.github.ajalt.timberkt.Timber
+import com.squareup.leakcanary.LeakCanary
 import timber.log.Timber.DebugTree
 
 class VoictureApplication : Application() {
     override fun onCreate() {
         super.onCreate()
+        if (LeakCanary.isInAnalyzerProcess(this)) {
+            // This process is dedicated to LeakCanary for heap analysis.
+            // You should not init your app in this process.
+            return
+        }
+        LeakCanary.install(this)
         Timber.plant(DebugTree())
     }
 }
