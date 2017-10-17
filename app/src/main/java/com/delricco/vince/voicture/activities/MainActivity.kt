7cac3e2ffe2d14e3f83b95ea9fe349c7aecba532
vdelricco <vd12scbc@gmail.com>
2017-10-17 14:19:29 -0600

    Move project creation logic to activity

--- app/src/main/java/com/delricco/vince/voicture/activities/MainActivity.kt
+++ app/src/main/java/com/delricco/vince/voicture/activities/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.delricco.vince.voicture.activities
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v7.app.AppCompatActivity
@@ -9,12 +11,19 @@ import android.widget.Toast
 import com.delricco.vince.voicture.R
 import com.delricco.vince.voicture.VoictureApplication
 import com.delricco.vince.voicture.commons.sharedprefs.SavedProject
+import com.delricco.vince.voicture.intents.IntentKeys
+import com.delricco.vince.voicture.intents.Intents
 import com.delricco.vince.voicture.interfaces.implementations.SimpleVoictureProjectPacker
 import com.delricco.vince.voicture.ui.fragments.CreateProjectFragment
+import com.github.ajalt.timberkt.Timber
 import kotlinx.android.synthetic.main.activity_main.*
 import javax.inject.Inject
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), CreateProjectFragment.CreateProjectListener {
+    companion object {
+        val PICK_IMAGES = 1
+    }
+
     @Inject protected lateinit var savedProjectPrefs: SavedProject
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,6 +32,36 @@ class MainActivity : AppCompatActivity() {
         VoictureApplication.activityComponent.inject(this)
         setSupportActionBar(toolbar)
         changeFragment(CreateProjectFragment(), false)
+    }
+
+    override fun onProjectCreateClicked() {
+        createProject()
+    }
+
+    private fun createProject() {
+        startActivityForResult(Intent.createChooser(Intents.CHOOSE_MULTIPLE_PHOTOS, "Select Pictures"), PICK_IMAGES)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == PICK_IMAGES && resultCode == AppCompatActivity.RESULT_OK && data != null) {
+            val selectedImageUriList = ArrayList<Uri>()
+            when {
+                data.data != null -> {
+                    Timber.d { "Adding ${data.data} to selected image uri list" }
+                    selectedImageUriList.add(data.data)
+                }
+                data.clipData != null -> (0..(data.clipData.itemCount - 1)).mapTo(selectedImageUriList) {
+                    Timber.d { "Adding ${data.clipData.getItemAt(it).uri} to selected image uri list" }
+                    data.clipData.getItemAt(it).uri
+                }
+                else -> return
+            }
+
+            val createProjectIntent = Intent(this, ProjectCreationActivity::class.java)
+            createProjectIntent.putParcelableArrayListExtra(IntentKeys.SELECTED_IMAGE_URI_LIST, selectedImageUriList)
+            startActivity(createProjectIntent)
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
--- app/src/main/java/com/delricco/vince/voicture/ui/fragments/CreateProjectFragment.kt
+++ app/src/main/java/com/delricco/vince/voicture/ui/fragments/CreateProjectFragment.kt
@@ -1,23 +1,16 @@
 package com.delricco.vince.voicture.ui.fragments
 
-import android.content.Intent
-import android.net.Uri
 import android.os.Bundle
 import android.support.v4.app.Fragment
-import android.support.v7.app.AppCompatActivity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.delricco.vince.voicture.R
-import com.delricco.vince.voicture.activities.ProjectCreationActivity
-import com.delricco.vince.voicture.intents.IntentKeys.Companion.SELECTED_IMAGE_URI_LIST
-import com.delricco.vince.voicture.intents.Intents
-import com.github.ajalt.timberkt.Timber
 import kotlinx.android.synthetic.main.fragment_create_project.*
 
 class CreateProjectFragment : Fragment() {
-    companion object {
-        val PICK_IMAGES = 1
+    interface CreateProjectListener {
+       fun onProjectCreateClicked()
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
@@ -25,31 +18,7 @@ class CreateProjectFragment : Fragment() {
     }
 
     override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
-        createNewProjectCard.setOnClickListener {
-            startActivityForResult(Intent.createChooser(Intents.CHOOSE_MULTIPLE_PHOTOS, "Select Pictures"), PICK_IMAGES)
-        }
+        createNewProjectCard.setOnClickListener { (activity as CreateProjectListener).onProjectCreateClicked() }
         super.onViewCreated(view, savedInstanceState)
-    }
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode == PICK_IMAGES && resultCode == AppCompatActivity.RESULT_OK && data != null) {
-            val selectedImageUriList = ArrayList<Uri>()
-            when {
-                data.data != null -> {
-                    Timber.d { "Adding ${data.data} to selected image uri list" }
-                    selectedImageUriList.add(data.data)
-                }
-                data.clipData != null -> (0..(data.clipData.itemCount - 1)).mapTo(selectedImageUriList) {
-                    Timber.d { "Adding ${data.clipData.getItemAt(it).uri} to selected image uri list" }
-                    data.clipData.getItemAt(it).uri
-                }
-                else -> return
-            }
-
-            val createProjectIntent = Intent(activity, ProjectCreationActivity::class.java)
-            createProjectIntent.putParcelableArrayListExtra(SELECTED_IMAGE_URI_LIST, selectedImageUriList)
-            activity.startActivity(createProjectIntent)
-        }
     }
 }
